5d2fc10b739b2cf324d7d86ee03001d5d5ab70eb
hewzhew <hewzhew@bupt.edu.cn>
2026-05-16 10:34:00 +0800

    Move final Silent definitions

--- src/content/cards/silent/adrenaline.rs
+++ src/content/cards/silent/adrenaline.rs
@@ -1,6 +1,29 @@
+use crate::content::cards::{CardDefinition, CardId, CardRarity, CardTarget, CardType};
 use crate::runtime::action::{Action, ActionInfo, AddTo};
 use crate::runtime::combat::{CombatCard, CombatState};
 use smallvec::SmallVec;
+
+pub fn definition() -> CardDefinition {
+    CardDefinition {
+        id: CardId::Adrenaline,
+        name: "Adrenaline",
+        card_type: CardType::Skill,
+        rarity: CardRarity::Rare,
+        cost: 0,
+        base_damage: 0,
+        base_block: 0,
+        base_magic: 2,
+        target: CardTarget::SelfTarget,
+        is_multi_damage: false,
+        exhaust: true,
+        ethereal: false,
+        innate: false,
+        tags: &[],
+        upgrade_damage: 0,
+        upgrade_block: 0,
+        upgrade_magic: 0,
+    }
+}
 
 pub fn adrenaline_play(_state: &CombatState, card: &CombatCard) -> SmallVec<[ActionInfo; 4]> {
     smallvec::smallvec![
--- src/content/cards/silent/tactician.rs
+++ src/content/cards/silent/tactician.rs
@@ -1,6 +1,29 @@
+use crate::content::cards::{CardDefinition, CardId, CardRarity, CardTarget, CardType};
 use crate::runtime::action::{Action, ActionInfo, AddTo};
 use crate::runtime::combat::{CombatCard, CombatState};
 use smallvec::SmallVec;
+
+pub fn definition() -> CardDefinition {
+    CardDefinition {
+        id: CardId::Tactician,
+        name: "Tactician",
+        card_type: CardType::Skill,
+        rarity: CardRarity::Uncommon,
+        cost: -2,
+        base_damage: 0,
+        base_block: 0,
+        base_magic: 1,
+        target: CardTarget::None,
+        is_multi_damage: false,
+        exhaust: false,
+        ethereal: false,
+        innate: false,
+        tags: &[],
+        upgrade_damage: 0,
+        upgrade_block: 0,
+        upgrade_magic: 1,
+    }
+}
 
 pub fn tactician_play(_state: &CombatState, _card: &CombatCard) -> SmallVec<[ActionInfo; 4]> {
     smallvec::smallvec![]
