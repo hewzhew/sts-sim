600b8c98801ffd483cd6f60f4bef617cb17f369e
hewzhew <hewzhew@bupt.edu.cn>
2026-05-16 10:19:23 +0800

    Move Silent starter common definitions

--- src/content/cards/silent/quick_slash.rs
+++ src/content/cards/silent/quick_slash.rs
@@ -1,7 +1,30 @@
+use crate::content::cards::{CardDefinition, CardId, CardRarity, CardTarget, CardType};
 use crate::core::EntityId;
 use crate::runtime::action::{Action, ActionInfo, AddTo, DamageInfo, DamageType};
 use crate::runtime::combat::{CombatCard, CombatState};
 use smallvec::SmallVec;
+
+pub fn definition() -> CardDefinition {
+    CardDefinition {
+        id: CardId::QuickSlash,
+        name: "Quick Slash",
+        card_type: CardType::Attack,
+        rarity: CardRarity::Common,
+        cost: 1,
+        base_damage: 8,
+        base_block: 0,
+        base_magic: 0,
+        target: CardTarget::Enemy,
+        is_multi_damage: false,
+        exhaust: false,
+        ethereal: false,
+        innate: false,
+        tags: &[],
+        upgrade_damage: 4,
+        upgrade_block: 0,
+        upgrade_magic: 0,
+    }
+}
 
 pub fn quick_slash_play(
     state: &CombatState,
--- src/content/cards/silent/survivor.rs
+++ src/content/cards/silent/survivor.rs
@@ -1,6 +1,29 @@
+use crate::content::cards::{CardDefinition, CardId, CardRarity, CardTarget, CardType};
 use crate::runtime::action::{Action, ActionInfo, AddTo};
 use crate::runtime::combat::{CombatCard, CombatState};
 use smallvec::SmallVec;
+
+pub fn definition() -> CardDefinition {
+    CardDefinition {
+        id: CardId::Survivor,
+        name: "Survivor",
+        card_type: CardType::Skill,
+        rarity: CardRarity::Basic,
+        cost: 1,
+        base_damage: 0,
+        base_block: 8,
+        base_magic: 0,
+        target: CardTarget::SelfTarget,
+        is_multi_damage: false,
+        exhaust: false,
+        ethereal: false,
+        innate: false,
+        tags: &[],
+        upgrade_damage: 0,
+        upgrade_block: 3,
+        upgrade_magic: 0,
+    }
+}
 
 pub fn survivor_play(_state: &CombatState, card: &CombatCard) -> SmallVec<[ActionInfo; 4]> {
     smallvec::smallvec![
