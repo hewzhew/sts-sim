1ae61d7872e487b0190919561629bf92ead16104
hewzhew <hewzhew@bupt.edu.cn>
2026-05-16 10:30:14 +0800

    Move Silent power and rare definitions

--- src/content/cards/silent/escape_plan.rs
+++ src/content/cards/silent/escape_plan.rs
@@ -1,6 +1,29 @@
+use crate::content::cards::{CardDefinition, CardId, CardRarity, CardTarget, CardType};
 use crate::runtime::action::{Action, ActionInfo, AddTo};
 use crate::runtime::combat::{CombatCard, CombatState};
 use smallvec::SmallVec;
+
+pub fn definition() -> CardDefinition {
+    CardDefinition {
+        id: CardId::EscapePlan,
+        name: "Escape Plan",
+        card_type: CardType::Skill,
+        rarity: CardRarity::Uncommon,
+        cost: 0,
+        base_damage: 0,
+        base_block: 3,
+        base_magic: 0,
+        target: CardTarget::SelfTarget,
+        is_multi_damage: false,
+        exhaust: false,
+        ethereal: false,
+        innate: false,
+        tags: &[],
+        upgrade_damage: 0,
+        upgrade_block: 2,
+        upgrade_magic: 0,
+    }
+}
 
 pub fn escape_plan_play(state: &CombatState, card: &CombatCard) -> SmallVec<[ActionInfo; 4]> {
     let evaluated = crate::content::cards::evaluate_card_for_play(card, state, None);
--- src/content/cards/silent/leg_sweep.rs
+++ src/content/cards/silent/leg_sweep.rs
@@ -1,8 +1,31 @@
+use crate::content::cards::{CardDefinition, CardId, CardRarity, CardTarget, CardType};
 use crate::content::powers::PowerId;
 use crate::core::EntityId;
 use crate::runtime::action::{Action, ActionInfo, AddTo};
 use crate::runtime::combat::{CombatCard, CombatState};
 use smallvec::SmallVec;
+
+pub fn definition() -> CardDefinition {
+    CardDefinition {
+        id: CardId::LegSweep,
+        name: "Leg Sweep",
+        card_type: CardType::Skill,
+        rarity: CardRarity::Uncommon,
+        cost: 2,
+        base_damage: 0,
+        base_block: 11,
+        base_magic: 2,
+        target: CardTarget::Enemy,
+        is_multi_damage: false,
+        exhaust: false,
+        ethereal: false,
+        innate: false,
+        tags: &[],
+        upgrade_damage: 0,
+        upgrade_block: 3,
+        upgrade_magic: 1,
+    }
+}
 
 pub fn leg_sweep_play(
     state: &CombatState,
