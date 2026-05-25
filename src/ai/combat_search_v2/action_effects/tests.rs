a3a9bb87b119de8a05638d23a79a59de0dcd9b7b
hewzhew <hewzhew@bupt.edu.cn>
2026-05-25 13:57:50 +0800

    Split action effect power signals

--- src/ai/combat_search_v2/action_effects/tests.rs
+++ src/ai/combat_search_v2/action_effects/tests.rs
@@ -1,6 +1,7 @@
 use super::*;
 use crate::content::cards::CardId;
 use crate::content::monsters::EnemyId;
+use crate::content::powers::PowerId;
 use crate::runtime::combat::{CombatCard, Power, PowerPayload};
 use crate::test_support::{blank_test_combat, test_monster};
 
